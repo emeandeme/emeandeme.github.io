08a799038cc3e8072cb4afed4fa0de8d38059a27
Cayetano Bañón <cayetano.banon@gmail.com>
2024-02-19 21:14:30 +0100

    hotfix: flutter web form

--- emeandeme_flutter/lib/widgets/form_section.dart
+++ emeandeme_flutter/lib/widgets/form_section.dart
@@ -1,23 +1,30 @@
 import 'package:emeandeme/widgets/sections/template_section.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class FormSection extends StatelessWidget with MediaQueryLayaouts {
+import 'package:webviewx/webviewx.dart';
+
+class FormSection extends StatefulWidget with MediaQueryLayaouts {
   const FormSection({super.key});
+
+  @override
+  State<FormSection> createState() => _FormSectionState();
+}
+
+class _FormSectionState extends State<FormSection> {
+  late WebViewXController webviewController;
 
   @override
   Widget build(BuildContext context) {
     return TemplateSectionSliver(
       child: Padding(
-        padding: EdgeInsets.symmetric(
-            horizontal: isSmallScreen(context)
-                ? 64
-                : (MediaQuery.of(context).size.width * 0.23),
-            vertical: 64),
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-              url: WebUri(
-                  "https://docs.google.com/forms/d/e/1FAIpQLScUQE7onIpfGpl1hFmEtJcU4k2frkf0XjEVrDe07HXLJoAUcA/viewform?embedded=true")),
+        padding: const EdgeInsets.symmetric(vertical: 60),
+        child: WebViewX(
+          width: (MediaQuery.of(context).size.width * 0.77),
+          height: 300,
+          onWebViewCreated: (controller) => webviewController = controller,
+          initialSourceType: SourceType.url,
+          initialContent:
+              "https://docs.google.com/forms/d/e/1FAIpQLScUQE7onIpfGpl1hFmEtJcU4k2frkf0XjEVrDe07HXLJoAUcA/viewform?embedded=true",
         ),
       ),
     );
