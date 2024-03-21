3cc26fbb1fc16cc2b166cb1f59200d6dcc76bcfb
Cayetano Bañón <cayetano.banon@gmail.com>
2024-03-21 19:54:44 +0100

    Change dependece

--- emeandeme_flutter/lib/widgets/form_section.dart
+++ emeandeme_flutter/lib/widgets/form_section.dart
@@ -1,5 +1,9 @@
+import 'package:emeandeme/module_structure/actions.dart';
+import 'package:emeandeme/module_structure/launch_bean.dart';
+import 'package:emeandeme/widgets/iframe.dart';
 import 'package:emeandeme/widgets/sections/template_section.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:webviewx_plus/webviewx_plus.dart';
 
 class FormSection extends StatefulWidget with MediaQueryLayaouts {
@@ -15,19 +19,37 @@ class _FormSectionState extends State<FormSection> {
   @override
   Widget build(BuildContext context) {
     return TemplateSectionSliver(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 60),
-        child: Center(
-          child: WebViewX(
-            width: (MediaQuery.of(context).size.width * 0.77),
-            height: widget.obtainTemplateSize(context),
-            onWebViewCreated: (controller) => webviewController = controller,
-            initialSourceType: SourceType.url,
-            initialContent:
-                "https://docs.google.com/forms/d/e/1FAIpQLScUQE7onIpfGpl1hFmEtJcU4k2frkf0XjEVrDe07HXLJoAUcA/viewform?embedded=true",
+      preferrizeHeight: 1200,
+      child: Center(
+          child: Column(
+        children: [
+          const Expanded(child: IframeScreen()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            child: ActionInk(
+                onTap: () => LaunchBean(
+                    uri: Uri.parse(
+                        "https://docs.google.com/forms/d/e/1FAIpQLScUQE7onIpfGpl1hFmEtJcU4k2frkf0XjEVrDe07HXLJoAUcA/viewform?embedded=true")),
+                child: const ColoredBox(
+                  color: Colors.green,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text('Formulario'),
+                  ),
+                )),
           ),
-        ),
-      ),
+        ],
+      )
+
+          // WebViewX(
+          //   width: (MediaQuery.of(context).size.width * 0.77),
+          //   height: widget.obtainTemplateSize(context),
+          //   onWebViewCreated: (controller) => webviewController = controller,
+          //   initialSourceType: SourceType.url,
+          //   initialContent:
+          //       "https://docs.google.com/forms/d/e/1FAIpQLScUQE7onIpfGpl1hFmEtJcU4k2frkf0XjEVrDe07HXLJoAUcA/viewform?embedded=true",
+          // ),
+          ),
     );
   }
 }
